a295af4ab6816c8a9ea05c7d2053698d96ed7901
Nico Kurniawan <nicokurniawan9@gmail.com>
2015-11-15 01:50:51 +0000

    test filter and sorting

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,15 @@
 class MoviesController < ApplicationController
+  before_action :set_filter, only: :index
 
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
 
+  def set_filter
+    session[:sort] = {} unless session[:sort]  
+    session[:filter] = {} unless session[:filter]  
+  end
+  
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -11,7 +17,24 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    
+    if params[:sort]
+      params[:sort].split('|').each do |column_name|
+        if Movie.column_names.include? column_name
+          session[:sort][column_name.to_sym] = :asc
+        end
+      end
+    end
+    
+    if params[:ratings] and not params[:ratings].length.zero?
+      session[:filter] = {'rating' => params[:ratings].keys}
+    end
+
+    @sort = session[:sort]
+    @filter = session[:filter]
+    @all_ratings = ['G', 'PG', 'PG-13', 'R'];
+    @movies = Movie.where(@filter).order(@sort)
+    
   end
 
   def new
